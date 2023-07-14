23f5df0d7e7a65ed782ec497a0a81e6cb6cde512
tristansilerio <tristanjsilerio@gmail.com>
2023-07-14 15:59:27 -0400

    added transitions/animations, fixed projects formatting a bit

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,7 +71,20 @@ jQuery(document).ready(function($) {
 		step();
 	  }
 	  
-	  
+	
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			console.log(entry)
+			if (entry.isIntersecting) {
+				entry.target.classList.add('show');
+			} else {
+				entry.target.classList.remove('show');
+			}
+		});
+	})
+
+	const hiddenElements = document.querySelectorAll('.hidden');
+	hiddenElements.forEach((el) => observer.observe(el));
 	  
   
   });
